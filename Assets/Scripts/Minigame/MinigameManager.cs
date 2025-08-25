b9de05ce11f46967e9828a6f96b5c430031495bc
Vinicius Renó <vinicius.vbreno@gmail.com>
2025-08-25 18:00:46 -0300

    Add Tree interaction with attempts and respawn

---
+++ Assets/Scripts/Minigame/MinigameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/// <summary>
+/// Handles launching of minigames.  Currently a placeholder.
+/// </summary>
+public class MinigameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Begin a minigame for the supplied tree.
+    /// </summary>
+    public static void StartGame(Tree tree)
+    {
+        // Minigame logic will be implemented later.
+    }
+}
