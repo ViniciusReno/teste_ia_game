00d0b234188c8b10527b074b9451ccec96bf18d7
Vinicius Renó <vinicius.vbreno@gmail.com>
2025-08-25 17:54:21 -0300

    Add persistent inventory singleton

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory Instance { get; private set; }
+
+    public int woodCount;
+
+    public event Action<int> OnWoodChanged;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddWood(int amount)
+    {
+        woodCount += amount;
+        OnWoodChanged?.Invoke(woodCount);
+    }
+
+    public void RemoveWood(int amount)
+    {
+        woodCount = Mathf.Max(0, woodCount - amount);
+        OnWoodChanged?.Invoke(woodCount);
+    }
+}
